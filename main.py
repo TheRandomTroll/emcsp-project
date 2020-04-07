d3eea23da1f94dd220b9e7467e9fb9532aee2887
TheRandomTroll <mihailgerginov01@gmail.com>
2020-04-07 18:49:09 +0300

    [Update] Added continuous frame capture

--- main.py
+++ main.py
@@ -7,23 +7,27 @@ import numpy as np
 import picamera
 
 
-def detect_bright_spots():
+def detect_bright_spots(image):
+
     return
 
 
 def main():
-    camera = picamera.PiCamera()
+    camera = PiCamera()
     camera.color_effects = (128, 128)
+    camera.resolution = (640, 480)
+    camera.framerate = 32
+    raw_capture = PiRGBArray(camera, size=(640, 480))
 
-    camera.start_preview()
-    print("Warming up camera...")
     sleep(0.1)
-    print("Camera initialized.")
-    while True:
-        print("Capturing image...")
-        camera.capture("foo.jpg")
-        print("Detecting bright spots...")
-        detect_bright_spots()
+    print("Camera initialized")
+    for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
+        image = frame.array
+        cv2.imshow("Frame", image)
+        key = cv2.waitKey(1) & 0xFF
 
+        raw_capture.truncate(0)
+        if key == ord("q"):
+            break
 if __name__ == "__main__":
     main()
