93b0b3f9cda1e99b809999c8403b796fc6029d41
TheRandomTroll <mihailgerginov01@gmail.com>
2020-04-07 19:04:45 +0300

    [Add] Finished light source detector

--- main.py
+++ main.py
@@ -8,13 +8,19 @@ import picamera
 
 
 def detect_bright_spots(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (11, 11), 0)
 
-    return
+    threshold = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)[1]
+    threshold = cv2.erode(threshold, None, iterations=2)
+    threshold = cv2.dilate(threshold, None, iterations=4)
 
+    cv2.namedWindow("Output")
+    cv2.moveWindow("Output", 700, 30)
+    cv2.imshow("Output", threshold)
 
 def main():
     camera = PiCamera()
-    camera.color_effects = (128, 128)
     camera.resolution = (640, 480)
     camera.framerate = 32
     raw_capture = PiRGBArray(camera, size=(640, 480))
@@ -23,7 +29,11 @@ def main():
     print("Camera initialized")
     for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
         image = frame.array
+        detect_bright_spots(image)
+        cv2.namedWindow("Frame")
+        cv2.moveWindow("Frame", 40, 30)
         cv2.imshow("Frame", image)
+
         key = cv2.waitKey(1) & 0xFF
 
         raw_capture.truncate(0)
